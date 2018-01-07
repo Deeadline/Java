9d8941b98ea5edbb42b0ab1cf6a17a7524ba2f72
Deeadline <mikszym95@gmail.com>
2018-01-07 19:22:39 +0100

    It works!

--- junitTest/src/main/java/DartGameFirst/DartGame.java
+++ junitTest/src/main/java/DartGameFirst/DartGame.java
@@ -15,42 +15,32 @@ public class DartGame {
 
     public void shoot(int points, int multiplier, int playerTurn) {
         validateData(points, multiplier);
-        if(playerTurn == 1) {
+        sampleShoot(points, multiplier, playerTurn);
+        checkWinner();
+    }
+    private void sampleShoot(int points, int multiplier, int playerTurn){
+        if (playerTurn == 1) {
             firstShooterPoints = countPoints(points, multiplier, getFirstShooterPoints());
-            if(isWinner(getFirstShooterPoints())) {
-                System.out.println("First player is won!");
-            }
-        }
-        else {
+        } else {
             secondShooterPoints = countPoints(points, multiplier, getSecondShooterPoints());
-            System.out.println(secondShooterPoints);
-            if(isWinner(getSecondShooterPoints())) {
-                System.out.println("Second player is won!");
-            }
         }
     }
-
     private int countPoints(int value, int multiplier, int points) {
         points -= value * multiplier;
-        if(points == 0) {
-            return 1000;
-        }
-        if(points < 0) {
-            return points + (value * multiplier);
-        }
         return points;
     }
 
-    private boolean isWinner(int points) {
-        return (points == 1000) ? true : false;
+    private void checkWinner() {
+        if (firstShooterPoints == 0)
+            System.out.println("First player won!");
+        if (secondShooterPoints == 0)
+            System.out.println("Second player won!");
     }
 
     private void validateData(int points, int multiplier) throws IllegalArgumentException {
-        if(points < 0 || points > 20) {
+        if (points < 0 || points > 20)
             throw new IllegalArgumentException("Illegal value of points!");
-        }
-        if(multiplier != 1 && multiplier != 2 && multiplier != 3) {
+        if (multiplier != 1 && multiplier != 2 && multiplier != 3)
             throw new IllegalArgumentException("Illegal value of multiplier!");
-        }
     }
 }
--- junitTest/src/test/java/DartGameFirst/DartGameTest.java
+++ junitTest/src/test/java/DartGameFirst/DartGameTest.java
@@ -1,74 +1,103 @@
 package DartGameFirst;
 
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 
+import static com.sun.deploy.trace.Trace.flush;
 import static org.junit.Assert.*;
 
 public class DartGameTest {
 
-    DartGame dart = new DartGame();
+    static private DartGame dart;
 
     @BeforeClass
-    public static void testInitialization() {
-        DartGame dart = new DartGame();
+    public static void setUpClass() {
+        dart = new DartGame();
         assertNotNull(dart);
     }
-
-    @Test
-    public void testGetFirstShooterPoints() throws Exception {
-        assertEquals(501, dart.getFirstShooterPoints());
+    static int value, value2;
+    @After
+    public void update(){
+        value = dart.getFirstShooterPoints();
+        value2 = dart.getSecondShooterPoints();
     }
 
     @Test
-    public void testGetSecondShooterPoints() throws Exception {
-        assertEquals(501, dart.getSecondShooterPoints());
+    public void testGetFirstShooterPoints() {
+        assertEquals(value, dart.getFirstShooterPoints());
     }
+
+    @Test
+    public void testGetSecondShooterPoints() {
+        assertEquals(value2, dart.getSecondShooterPoints());
+    }
+
     @Test(expected = IllegalArgumentException.class)
-    public void shootTestIllegalArgumentException(){
-        dart.shoot(200,3,1);
-    }
-    @Test
-    public void testShoot() throws Exception {
-        dart.shoot(20, 1, 1);
-        assertEquals(481, dart.getFirstShooterPoints());
-
-        dart.shoot(20, 1, 2);
-        assertEquals(481, dart.getSecondShooterPoints());
-
-        dart.shoot(20, 3, 2);
-        dart.shoot(20, 3, 2);
-        dart.shoot(20, 3, 2);
-        dart.shoot(20, 3, 2);
-        dart.shoot(20, 3, 2);
-        dart.shoot(20, 3, 2);
-        dart.shoot(20, 3, 2);
-        dart.shoot(20, 3, 2);
-        dart.shoot(2,1,2);
-        dart.shoot(1,1,2);
-
-        dart.shoot(20, 3, 1);
-        dart.shoot(20, 3, 1);
-        dart.shoot(20, 3, 1);
-        dart.shoot(20, 3, 1);
-        dart.shoot(20, 3, 1);
-        dart.shoot(20, 3, 1);
-        dart.shoot(20, 3, 1);
-        dart.shoot(20, 3, 1);
-        dart.shoot(2,1,1);
-        dart.shoot(1,1,1);
-
-    }
-
-    @Test (expected = IllegalArgumentException.class)
     public void testShootTestPointsIllegalArgumentException() {
         dart.shoot(200, 2, 1);
     }
 
-    @Test (expected = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testShootTestMultiplierIllegalArgumentException() {
         dart.shoot(20, 4, 2);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void shootTestIllegalArgumentException() {
+        dart.shoot(200, 3, 1);
+    }
+
+    @Test
+    public void testShoot() {
+        dart.shoot(20, 3, 1);
+        assertEquals(441, dart.getFirstShooterPoints());
+    }
+
+    @Test
+    public void testShoot1() {
+        dart.shoot(20, 3, 1);
+        assertEquals(381, dart.getFirstShooterPoints());
+    }
+
+    @Test
+    public void testShoot2() {
+        dart.shoot(20, 3, 1);
+        assertEquals(321, dart.getFirstShooterPoints());
+    }
+
+    @Test
+    public void testShoot3() {
+        dart.shoot(20, 3, 1);
+        assertEquals(261, dart.getFirstShooterPoints());
+    }
+
+    @Test
+    public void testShoot4() {
+        dart.shoot(20, 3, 1);
+        assertEquals(201, dart.getFirstShooterPoints());
+    }
+
+    @Test
+    public void testShoot5() {
+        dart.shoot(20, 3, 1);
+        assertEquals(141, dart.getFirstShooterPoints());
+    }
+
+    @Test
+    public void testShoot6() {
+        dart.shoot(20, 3, 1);
+        assertEquals(81, dart.getFirstShooterPoints());
+    }
+
+    @Test
+    public void testShoot7() {
+        dart.shoot(15, 3, 1);
+        assertEquals(36, dart.getFirstShooterPoints());
+    }
+
+    @Test
+    public void testShoot8() {
+        dart.shoot(18, 2, 1);
+        assertEquals(0, dart.getFirstShooterPoints());
+    }
 
 }
